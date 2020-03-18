628e54298d2785b8a639618e4e690cb55ffca2bd
programando <jhonjamesmg@hotmail.com>
2020-03-17 19:03:21 -0500

    Inicio proyecto Nuxt

---
+++ config/env.js
@@ -0,0 +1,3 @@
+export default {
+   api: 'http://balquimia-erp-server-api-laravel-7.local/api/',
+}
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <h1> Facturas creadas</h1>
+      <div v-for="Factura in Facturas" :key="Factura.id_fact_elctrnca">
+          <h2> {{ Factura.fcha_rgstro }}</h2>
+      </div>
+      
+  </div>
+</template>
+
+<script>
+ 
+ import axios from 'axios';
+ import env    from '@/config/env';
+
+export default {
+  name: 'IndexPage',
+  data: () => ({
+    Facturas: [],
+  }),
+  created: async function() {
+    let response = await axios.get(`${env.api}facturas`);
+    this.Facturas = response.data;
+    console.log ( this.Facturas);
+  }
+}
+</script>
+
+<style>
+
+</style>
