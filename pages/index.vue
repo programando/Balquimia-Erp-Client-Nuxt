c1eb2822119046f7dc819d5b49931eb45622ee2b
programando <jhonjamesmg@hotmail.com>
2020-03-26 11:47:39 -0500

    Consulta datos

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,9 @@
    
     <h1> Facturas creadas</h1>
        <h2>{{ Mensaje }} </h2>
-<!--       <div v-for="Factura in Facturas" :key="Factura.id_fact_elctrnca">
+       <div v-for="Factura in Facturas" :key="Factura.id_fact_elctrnca">
           <h2> {{ Factura.fcha_rgstro }}</h2>
-      </div> -->
+      </div>  
       
   </div>
 </template>
@@ -19,11 +19,11 @@ export default {
   name: 'IndexPage',
   data: () => ({
     Facturas: [],
-    Mensaje:'He cargado la página inicial',
+    Mensaje:'He cargado con las facturas creadas. 10 primeras facturas.',
   }),
   created: async function() {
-    //let response = await axios.get(`${env.api}facturas`);
-    //this.Facturas = response.data;
+    let response = await axios.get(`${env.api}facturas`);
+    this.Facturas = response.data;
     
   }
 }
