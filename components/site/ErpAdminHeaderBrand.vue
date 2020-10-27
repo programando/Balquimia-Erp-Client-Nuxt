d62ae9201b1e8a1b4efb6633243ee06df61b1785
programando <jhonjamesmg@hotmail.com>
2020-10-26 20:02:04 -0500

    Title-Brand

--- components/site/ErpAdminHeaderBrand.vue
+++ components/site/ErpAdminHeaderBrand.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="nombre-sitio"> 
-  <h1 class="escritorio text-3xl">AdminFlex</h1>
-  <h1 class="movil text-3xl">AF</h1>
+  <h1 class="escritorio text-3xl">BALQUIMIA</h1>
+  <h1 class="movil text-3xl">B.Q.</h1>
   </div>  
 </template>
