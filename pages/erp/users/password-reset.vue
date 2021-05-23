d83aff6edde1204604afc1f84e410148d86b1bd1
programando <jhonjamesmg@hotmail.com>
2021-05-22 19:55:01 -0500

    cambios

--- pages/erp/users/password-reset.vue
+++ pages/erp/users/password-reset.vue
@@ -1,11 +1,11 @@
 <template>
 <div >
   <form >
-      <div class="signup-1 flex items-center relative h-screen" >
-        <div class="overlay absolute inset-0 z-0 bg-black opacity-50"></div>
-            <div class="container px-4 mx-auto relative z-10">
-                <div class="sm:w-10/12 md:w-8/12 lg:w-6/12 xl:w-5/12 mx-auto">
-                    <div class="box bg-white p-6 md:px-12 md:pt-12 border-t-10 border-solid border-indigo-600" >
+      <div class="relative flex items-center h-screen signup-1" >
+        <div class="absolute inset-0 z-0 bg-black opacity-50 overlay"></div>
+            <div class="container relative z-10 px-4 mx-auto">
+                <div class="mx-auto sm:w-10/12 md:w-8/12 lg:w-6/12 xl:w-5/12">
+                    <div class="p-6 bg-white border-indigo-600 border-solid box md:px-12 md:pt-12 border-t-10" >
                       <div v-if="alert">
                             <SimpleAlert 
                                 title="Proceso finalizado !"
@@ -17,26 +17,26 @@
                       </div>
                      
                        <div v-if="!alert">
-                            <h2 class="text-3xl text-gray-800 text-center"> BALQUIMIA S.A.S.</h2>
-                            <p class="text-sm text-center mt-4">
+                            <h2 class="text-3xl text-center text-gray-800"> BALQUIMIA S.A.S.</h2>
+                            <p class="mt-4 text-sm text-center">
                                 Enviaremos un correo electrónico a la cuenta registrada en nuestro sistema con las con las instrucciones para que cambie los datos de acceso.
                             </p>
-                          <div class="signup-form mt-6 md:mt-6">
-                                <div class=" border border-gray-400 rounded flex items-center">
-                                  <div class="w-10 h-7 flex justify-center items-center flex-shrink-0 ">
+                          <div class="mt-6 signup-form md:mt-6">
+                                <div class="flex items-center border border-gray-400 rounded ">
+                                  <div class="flex items-center justify-center flex-shrink-0 w-10 h-7 ">
                                     <span class="text-black"> 
                                     <i class="fa fa-envelope fa-2x " aria-hidden="true"></i>
                                     </span>
-                                </div>
+                                 </div>
                                 <div class="flex-1 ">
-                                  <input  class="h-7 py-1 pr-3 w-full  focus:outline-none px-4" 
+                                  <input  class="w-full px-4 py-1 pr-3 h-7 focus:outline-none" 
                                       id="email" type="email" placeholder="cuenta de correo electrónico o email..." autofocus
                                       v-model="form.email" />                 
                                 </div>
                               </div>
                               <ErrorMessage v-if="errors.email"> {{ errors.email[0] }}  </ErrorMessage>
                         </div>
-                        <div class="text-center mt-6 md:mt-12">
+                        <div class="mt-6 text-center md:mt-12">
                             <ButtonLoading 
                                 @click.prevent="ResetPassword" 
                                   size="small" 
@@ -53,7 +53,7 @@
     </form>
 </div>
 
-<!--          <button class="bg-orange-600 hover:bg-orange-400 text-white text-md py-2 px-4 md:px-6 rounded transition-colors duration-300"
+<!--          <button class="px-4 py-2 text-white transition-colors duration-300 bg-orange-600 rounded hover:bg-orange-400 text-md md:px-6"
               @click.prevent="ResetPassword">
                 Enviar correo 
             </button> 
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,129 @@
 <template>
-  <div class="">
-      <nav class="flex flex-wrap justify-between items-center fixed w-full mt-0">
-        <nuxt-link to="/">
-         <div class="">
-          
-            <div class="flex flex-col w-64 ml-3">
-                <img class="relaive " style="z-index:2" src="/images/site/logo-balquimia.png" alt="" >
-                <img class="absolute mt-20 w-1/3" style=" z-index:1"  src="/images/site/logo-balquimia-arrow-down.png" alt="">
+<div class="flex items-center justify-center h-screen overflow-hidden" style="background: #edf2f7;">
+
+    <header class="w-full h-full bg-center bg-cover opacity-100" 
+            style="background-image: url('/images/login/background-login.jpg');">
+            <h1 class="mt-12 text-4xl font-bold tracking-wide text-center text-white">BALQUIMIA S.A.S.</h1>
+      
+        <div class="px-40 py-2 content">
+            <div class="mx-8 mt-20 body">
+                <div class="items-center justify-between md:flex">
+                   
+                    <div class="w-full mr-auto md:w-1/2" style="text-shadow: 0 20px 50px hsla(0,0%,0%,8);">                     
+                            <Frases-Celebres></Frases-Celebres>
+                    </div>
+
+                    <div class="w-full mt-6 md:max-w-md">
+                        <div class="px-4 py-4 mb-6 bg-white rounded-lg shadow-md card ">
+                            <form >
+                                <div class="flex items-center justify-center">
+                                    <h2 class="text-2xl font-bold tracking-wide">
+                                        Bienvenid@
+                                    </h2> 
+                                </div>
+                                <h5 class="mb-2 text-sm font-semibold text-center text-gray-800">
+                                   Registre sus credenciales para ingreso al sistema
+                                </h5>
+ 
+                           
+                                <InputBasic type= "email"
+                                        placeholder="Dirección electrónica (Email)"
+                                        v-model="form.email" 
+                                        :errors="errors.email"
+                                        @keyup="clearErrors"
+                                > </InputBasic>
+
+                                <InputBasic type= "password"
+                                        class="mt-6"
+                                        placeholder="Password o contraseña"
+                                        v-model="form.password" 
+                                        @keyup="clearErrors"
+                                > </InputBasic>
+
+
+                                <div class="flex items-center justify-between mt-3">
+                                  
+                                    <nuxt-link to="/erp/users/password-reset" class="text-gray-600"> 
+                                            Olvidé mi contraseña ? 
+                                    </nuxt-link>
+
+                                    <ButtonLoading 
+                                        @click.prevent="login" 
+                                        size="small" 
+                                        ref="ButtonLoading" 
+                                        variant="success"
+                                        variant-type="normal">  Ingresar al sistema 
+                                    </ButtonLoading>
+
+                                </div>
+                                
+                            </form>
+                        </div>
+                        
+                    </div>
+
+                </div>
             </div>
-        
         </div>
-          </nuxt-link>
-          
-         <div>
-              <nuxt-link to="/" class="mr-3 p-4" > Inicio</nuxt-link>
-              <nuxt-link to="/" class="mr-3 p-4" > Productos</nuxt-link>
-              <nuxt-link to="/" class="mr-3 p-4" > Servicios</nuxt-link>
-              <nuxt-link to="/" class="mr-3 p-4" > Experiencia</nuxt-link>
-              <nuxt-link to="/" class="mr-3 p-4" > Contacto</nuxt-link>
-         </div>
-         <div>
-            <div class="flex ml-2 ">
-              <img src="/images/flags/en.png" alt="" class="mr-2 p-3">
-              <img src="/images/flags/en.webp" alt="" class="mr-2 p-3">
-            </div>
-          </div>
-      </nav>
-        <div class="w-full mt-20">
-          <VueSlickCarousel :arrows="true"  v-bind="settings">
-            <div class="w-1/2">
-                <img src="/images/slides/home-1.webp" alt="">
-            </div>
-            <div class="w-1/2"><img src="/images/slides/home-2.webp" alt=""></div>
-            <div class="w-1/2"><img src="/images/slides/home-3.webp" alt=""></div>
-            <div class="w-1/2"><img src="/images/slides/home-4.webp" alt=""></div>
-          </VueSlickCarousel>  
-      </div>
-  </div>
+    </header>
+</div>
 </template>
 
 <script>
- import VueSlickCarousel from 'vue-slick-carousel'
-  import 'vue-slick-carousel/dist/vue-slick-carousel.css'
-  // optional style for arrows & dots
-  import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+  import User            from "@/models/User";
+  import FrasesCelebres  from "@/components/site/FrasesCelebresComponent";
+  import InputBasic      from "@/components/controls/inputs/Input-Basic";
+  import ButtonLoading   from "@/components/controls/buttons/ButtonLoading";
+  import Cookie          from 'js-cookie';
+  import {mapState,mapGetters} from 'vuex'; 
+ 
+ 
+  
+  export default {
+   
+  name: 'IndexPage',
 
-export default {
-  data: () =>({
-        settings:{},
+  data: () => ({
+      form: {
+        email: "jhonjamesmg@hotmail.com",
+        password: "123456"
+      },
+      errors: [ ],
+      buttonIsDisabled: false
   }),
-  components: { VueSlickCarousel },
+  components : { FrasesCelebres, InputBasic, ButtonLoading } ,
 
-  mounted(){
-      this.settings = {
-          "arrows": true,
-          "dots": true,
-          "infinite": true,
-          "slidesToShow": 1,
-          "slidesToScroll": 1,
-          "autoplay": true,
-          "speed": 8000,
-          "autoplaySpeed": 1000,
-          "cssEase": "linear",
-          "adaptiveHeight": true,
-          "focusOnSelect":true
-        
-}
-  },
+   middleware :  [ 'loggedIn'],
 
+   mounted() { 
+       User.getCokie()
+   },
+
+   methods: {
+      login() {
+        this.$refs.ButtonLoading.startLoading();
+         User.login( this.form)
+        .then (response => {
+            this.$store.commit('SET_USER', response.data);
+            this.$router.replace({ path: '/erp/facturas/listado' });
+            this.buttonIsDisabled = true;
+            this.$refs.ButtonLoading.stopLoading();
+        })
+        .catch( error => {
+          if ( error.response.status == 422) {
+            this.errors = error.response.data.errors;  
+            this.$refs.ButtonLoading.stopLoading(); 
+          }
+        })
+       
+      },
+
+      clearErrors() {
+          this.errors = [];
+          this.buttonIsDisabled = false;
+      },
+
+  }// Methods
 
 }
 </script>
